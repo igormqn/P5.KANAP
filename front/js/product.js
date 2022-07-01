c1002233e6cd5fbecff4ac79c0569c860065620d
igor maquin <igor.maquin@gmail.com>
2022-07-01 22:53:05 +0200

    changes javascript

---
+++ front/js/product.js
@@ -0,0 +1,9 @@
+if (qte < 1 || qte > 100)
+{ alert("Veuillez saisir une quantité entre 1 et 100"); 
+ return false;
+}
+colors.forEach(element =>  {
+    console.log(element)});
+function option(Value) {
+    let option = document.createElement("option")
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,18 +1,66 @@
 fetch("http://localhost:3000/api/products")
 .then((res) => res.json())
-.then((data) => {
+.then((data) => addProducts(data))
 
-        
-    });
+function addProducts (data) {
+    //const _id = data[0]._id
+   // const imageUrl = data[0].imageUrl
+   // const altTxt = data[0].altTxt
+   // const name = data[0].name
+   // const description = data[0].
+
+
+  data.forEach((canape) => {
     
-function addProduct(donnees) {
-    let imageUrl = donnees[0].imageUrl
-   
-    let anchor = document.createElement("a") 
-    anchor.text = "Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu."
-    anchor.href = imageUrl
-    let items = document.querySelector("#items")
-    if (items != null) {
-        items.appendChild(anchor)
-    }
+    const {_id, imageUrl, altTxt, name, description} = canape
+    const anchor = makeAnchor(_id)
+    const article = document.createElement("article")
+    const image = makeImage(imageUrl, altTxt)
+    const h3 = makeH3(name)
+    const p = makeParagraph(description)
+    
+    appendElementstoArticle(article, [image, h3, p])
+    appendArticleToAnchor(anchor, article)    
+})
+}
+function appendElementstoArticle(article, array) {
+    array.forEach((item) => {
+        article.appendChild(item)
+    })
+    article.appendChild(image) 
+    article.appendChild(h3) 
+    article.appendChild(p)
+}
+
+function makeAnchor(id) {
+    const anchor = document.createElement("a")
+    anchor.href = "./product.html?id=" + id
+    return anchor  
+}
+
+function appendArticleToAnchor(anchor, article)  {
+    const items = document.querySelector("#items")
+    if (items!= null) {
+        items.appendChild(anchor) 
+        anchor.appendChild(article)     
+    } 
+}
+function makeImage(imageUrl, altTxt) {
+    const image = document.createElement("img")
+    image.src = imageUrl
+    image.alt = altTxt
+    return image
+}
+
+function makeH3(name) {
+    const h3 = document.createElement("h3")
+    h3.textContent = name
+    h3.classList.add("productName")
+    return h3
+}
+function makeParagraph(description) {
+    const p = document.createElement("p")
+    p.textContent = description
+    p.classList.add("productDescription")
+    return p
 }
